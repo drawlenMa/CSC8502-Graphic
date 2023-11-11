50c2a3b3ad0329b6eb3f08504746f20c9d97858a
drawlenMa <110146356+drawlenMa@users.noreply.github.com>
2023-11-11 15:46:59 +0800

    Add files via upload

---
+++ Shaders/PerPixelFragment.glsl
@@ -0,0 +1,55 @@
+#version 330 core
+
+uniform sampler2D diffuseTex;
+uniform vec3 cameraPos;
+uniform vec4 lightColour;
+uniform vec3 lightPos;
+uniform float lightRadius;
+
+uniform vec4 lightColour1;
+uniform vec3 lightPos1;
+uniform float lightRadius1;
+
+in Vertex {
+vec4 colour;
+vec2 texCoord;
+vec3 normal;
+vec3 worldPos;
+} IN;
+
+out vec4 fragColour;
+void main(void) {
+{
+vec3 incident = normalize(lightPos - IN.worldPos );
+vec3 viewDir = normalize(cameraPos - IN.worldPos );
+vec3 halfDir = normalize(incident + viewDir );
+
+vec4 diffuse = texture(diffuseTex , IN.texCoord );
+float lambert = max(dot(incident , IN.normal), 0.0f);
+float distance = length(lightPos - IN.worldPos );
+float attenuation = 1.0 - clamp(distance / lightRadius , 0.0, 1.0);
+float specFactor = clamp(dot(halfDir , IN.normal ) ,0.0 ,1.0);
+specFactor = pow(specFactor , 60.0 );
+vec3 surface = (diffuse.rgb * lightColour.rgb);
+fragColour.rgb = surface * lambert * attenuation;
+fragColour.rgb += (lightColour.rgb * specFactor )* attenuation *0.33;
+fragColour.rgb += surface * 0.1f; // ambient!
+fragColour.a = diffuse.a;
+
+}
+vec3 incident = normalize(lightPos1 - IN.worldPos );
+vec3 viewDir = normalize(cameraPos - IN.worldPos );
+vec3 halfDir = normalize(incident + viewDir );
+
+vec4 diffuse = texture(diffuseTex , IN.texCoord );
+float lambert = max(dot(incident , IN.normal), 0.0f);
+float distance = length(lightPos1 - IN.worldPos );
+float attenuation = 1.0 - clamp(distance / lightRadius1 , 0.0, 1.0);
+float specFactor = clamp(dot(halfDir , IN.normal ) ,0.0 ,1.0);
+specFactor = pow(specFactor , 60.0 );
+vec3 surface = (diffuse.rgb * lightColour1.rgb);
+fragColour.rgb = surface * lambert * attenuation;
+fragColour.rgb += (lightColour1.rgb * specFactor )* attenuation *0.33;
+fragColour.rgb += surface * 0.1f; // ambient!
+fragColour.a = diffuse.a;
+}
